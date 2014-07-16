163c872d57dbfd814b10b081407886e2c837634f
Pronko <platon@pronko.ru>
2014-07-16 13:26:41 +0400

    started GradeOCR project. Implemented basic image viewer (with drag, zoom and scroll support)

---
+++ GradeOCR/PictureView.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Reflection;
+
+namespace GradeOCR {
+    public class PictureView : UserControl {
+
+        private Image _Image;
+        public Image Image {
+            get {
+                return _Image;
+            }
+            set {
+                _Image = value;
+                this.Invalidate();
+            }
+        }
+
+        private float zoom = 1;
+        private float offsetX = 0;
+        private float offsetY = 0;
+        
+        private HScrollBar hScrollBar = new HScrollBar();
+        private VScrollBar vScrollBar = new VScrollBar();
+        private Panel plug = new Panel();
+
+        public static readonly int scrollBarWidth = 15;
+
+        public PictureView() : base() {
+            this.BorderStyle = BorderStyle.FixedSingle;
+            this.DoubleBuffered = true;
+            //this.BackColor = Color.White;
+
+            _Image = LoadPlaceholder();
+            this.Controls.Add(plug);
+            hScrollBar.Minimum = 0;
+            hScrollBar.Maximum = 1000;
+            this.Controls.Add(hScrollBar);
+            vScrollBar.Minimum = 0;
+            vScrollBar.Maximum = 1000;
+            this.Controls.Add(vScrollBar);
+
+            this.SizeChanged += new EventHandler(delegate {
+                ClampOffsets();
+                this.PerformLayout();
+                this.Invalidate();
+            });
+
+            AddMouseScroll();
+            AddMouseDrag();
+            AddMouseZoom();
+        }
+
+        private void AddMouseScroll() {
+            hScrollBar.Scroll += new ScrollEventHandler(delegate {
+                offsetX = (_Image.Width - (this.Width - scrollBarWidth) / zoom) * ((float) hScrollBar.Value / 1000);
+                this.Invalidate();
+            });
+            vScrollBar.Scroll += new ScrollEventHandler(delegate {
+                offsetY = (_Image.Height - (this.Height - scrollBarWidth) / zoom) * ((float) vScrollBar.Value / 1000);
+                this.Invalidate();
+            });
+        }
+
+        private void AddMouseZoom() {
+            this.MouseWheel += new MouseEventHandler(delegate(object sender, MouseEventArgs e) {
+                float origZoom = zoom;
+                
+                for (int i = 0; i < e.Delta; i += 120) {
+                    zoom *= 1.05f;
+                }
+                for (int i = 0; i > e.Delta; i -= 120) {
+                    zoom *= 0.95f;
+                }
+
+                bool origFit = _Image.Width * origZoom <= this.Width && _Image.Height * origZoom <= this.Height;
+                bool fit = _Image.Width * zoom <= this.Width && _Image.Height * zoom <= this.Height;
+
+                if (fit) {
+                    // if image fits into view, no action is required
+                    offsetX = 0;
+                    offsetY = 0;
+                } else if (origFit && !fit) {
+                    // if image fit into frame before zoom, but stopped fitting after - need to center the image
+                    if (_Image.Width * zoom > this.Width - scrollBarWidth) {
+                        offsetX = (_Image.Width - (this.Width - scrollBarWidth) / zoom) / 2;
+                    } else {
+                        offsetX = 0;
+                    }
+                    if (_Image.Height * zoom > this.Height - scrollBarWidth) {
+                        offsetY = (_Image.Height - (this.Height - scrollBarWidth) / zoom) / 2;
+                    } else {
+                        offsetY = 0;
+                    }
+                    ClampOffsets();
+                } else {
+                    // image didn't fit before zoom or after
+                    if (_Image.Width * zoom <= this.Width - scrollBarWidth) {
+                        offsetX = 0;
+                    } else {
+                        offsetX += ((this.Width - scrollBarWidth) / origZoom - (this.Width - scrollBarWidth) / zoom) / 2;
+                    }
+                    if (_Image.Height * zoom <= this.Height - scrollBarWidth) {
+                        offsetY = 0;
+                    } else {
+                        offsetY += ((this.Height - scrollBarWidth) / origZoom - (this.Height - scrollBarWidth) / zoom) / 2;
+                    }
+                    ClampOffsets();
+                }
+
+                UpdateScrollBars();
+                
+                this.PerformLayout();
+                this.Invalidate();
+            });
+        }
+
+        private void AddMouseDrag() {
+            Point dragStart = new Point();
+            float startOffsetX = 0;
+            float startOffsetY = 0;
+
+            this.MouseDown += new MouseEventHandler(delegate(object sender, MouseEventArgs e) {
+                if (e.Button == MouseButtons.Left) {
+                    dragStart = e.Location;
+                    startOffsetX = offsetX;
+                    startOffsetY = offsetY;
+                    this.Cursor = Cursors.NoMove2D;
+                }
+            });
+            this.MouseMove += new MouseEventHandler(delegate(object sender, MouseEventArgs e) {
+                if (e.Button == MouseButtons.Left) {
+                    offsetX = startOffsetX + (dragStart.X - e.X) / zoom;
+                    offsetY = startOffsetY + (dragStart.Y - e.Y) / zoom;
+                    ClampOffsets();
+                    UpdateScrollBars();
+                    this.Invalidate();
+                }
+            });
+            this.MouseUp += new MouseEventHandler(delegate(object sender, MouseEventArgs e) {
+                if (e.Button == MouseButtons.Left) {
+                    this.Cursor = Cursors.Default;
+                }
+            });
+        }
+
+        public static Image placeholderImage = null;
+        public static Image LoadPlaceholder() {
+            if (placeholderImage == null) {
+                placeholderImage = Image.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("GradeOCR.hourglass.png"));
+            }
+            return placeholderImage;
+        }
+
+        protected override void OnLayout(LayoutEventArgs e) {
+            base.OnLayout(e);
+            if (_Image.Width * zoom <= this.Width && _Image.Height * zoom <= this.Height) {
+                // the whole image fits into component, no scroll bars required
+                vScrollBar.Visible = false;
+                hScrollBar.Visible = false;
+                plug.Visible = false;
+            } else {
+                // image doesn't fit into component, need to display scrolling gears
+                vScrollBar.Visible = true;
+                hScrollBar.Visible = true;
+                plug.Visible = true;
+
+                vScrollBar.Size = new Size(scrollBarWidth, this.Height - scrollBarWidth);
+                vScrollBar.Location = new Point(this.Width - scrollBarWidth - 2, 0);
+                hScrollBar.Size = new Size(this.Width - scrollBarWidth, scrollBarWidth);
+                hScrollBar.Location = new Point(0, this.Height - scrollBarWidth - 2);
+                plug.Size = new Size(scrollBarWidth, scrollBarWidth);
+                plug.Location = new Point(this.Width - scrollBarWidth, this.Height - scrollBarWidth);
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e) {
+            base.OnPaint(e);
+            if (zoom > 1) {
+                e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                e.Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.None;
+            } else {
+                e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Bilinear;
+                e.Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.Default;
+            }
+
+            if (_Image.Width * zoom <= this.Width && _Image.Height * zoom <= this.Height) {
+                float zw = _Image.Width * zoom;
+                float zh = _Image.Height * zoom;
+                float x = (this.Width - zw) / 2;
+                float y = (this.Height - zh) / 2;
+                e.Graphics.DrawImage(_Image, new RectangleF(x, y, zw, zh));
+            } else if (_Image.Height * zoom <= this.Height - scrollBarWidth) {
+                float destW = this.Width - scrollBarWidth;
+                float destH = _Image.Height * zoom;
+                float y = (this.Height - scrollBarWidth - destH) / 2;
+                e.Graphics.DrawImage(
+                    _Image,
+                    new RectangleF(0, y, destW, destH),
+                    new RectangleF(offsetX, 0, destW / zoom, _Image.Height),
+                    GraphicsUnit.Pixel
+                );
+            } else if (_Image.Width * zoom <= this.Width - scrollBarWidth) {
+                float destW = _Image.Width * zoom;
+                float x = (this.Width - scrollBarWidth - destW) / 2;
+                float destH = this.Height - scrollBarWidth;
+                e.Graphics.DrawImage(
+                    _Image,
+                    new RectangleF(x, 0, destW, destH),
+                    new RectangleF(0, offsetY, _Image.Width, destH / zoom),
+                    GraphicsUnit.Pixel
+                );
+            } else {
+                float destW = this.Width - scrollBarWidth;
+                float destH = this.Height - scrollBarWidth;
+                e.Graphics.DrawImage(
+                    _Image, 
+                    new RectangleF(0, 0, destW, destH),
+                    new RectangleF(offsetX, offsetY, destW / zoom, destH / zoom),
+                    GraphicsUnit.Pixel
+                );
+            }
+        }
+
+        private void UpdateScrollBars() {
+            hScrollBar.Value = (int) Math.Min(1000, Math.Max(0, hScrollBar.Maximum * (offsetX / (_Image.Width - (this.Width - scrollBarWidth) / zoom))));
+            vScrollBar.Value = (int) Math.Min(1000, Math.Max(0, vScrollBar.Maximum * (offsetY / (_Image.Height - (this.Height - scrollBarWidth) / zoom))));
+        }
+
+        private void ClampOffsets() {
+            offsetX = Math.Min(_Image.Width - (this.Width - scrollBarWidth) / zoom, Math.Max(0, offsetX));
+            offsetY = Math.Min(_Image.Height - (this.Height - scrollBarWidth) / zoom, Math.Max(0, offsetY));
+        }
+
+        public void ZoomToFit() {
+            float zoomX = (float) this.Width / (float) _Image.Width;
+            float zoomY = (float) this.Height / (float) _Image.Height;
+            zoom = Math.Min(zoomX, zoomY);
+            this.PerformLayout();
+            this.Invalidate();
+        }
+    }
+}
---
+++ GradeOCR/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GradeOCR {
+    class Program {
+        static void Main(string[] args) {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            Form form = new Form();
+            form.Size = new Size(600, 600);
+
+            PictureView pv = new PictureView();
+            pv.Location = new Point(0, 0);
+            pv.Size = new Size(584, 562);
+            pv.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Bottom;
+            form.Controls.Add(pv);
+
+            Image img = Image.FromFile("E:/ocr/scan1.jpg");
+            pv.Image = img;
+            pv.ZoomToFit();
+
+            Application.Run(form);
+        }
+
+    }
+}
