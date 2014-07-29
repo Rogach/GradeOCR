69366cad1874d85d74de26df891f945067945f29
Pronko <platon@pronko.ru>
2014-07-29 17:01:56 +0400

    added small app to automatically draw tables on a folder of images (for table OCR testing)

---
+++ TableRecognitionTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OCRUtil;
+using System.IO;
+using TableOCR;
+using System.Drawing;
+using LibUtil;
+
+namespace TableRecognitionTest {
+    class Program {
+        public static readonly string TableInputDir = "E:/Pronko/prj/Grader/ocr-data/register-bad";
+        public static readonly string TableOutputDir = "E:/Pronko/prj/Grader/ocr-data/register-bad-tables";
+
+        static void Main(string[] args) {
+            Util.Timed("table processing", () => {
+                var images = Directory.GetFiles(TableInputDir);
+                int c = 0;
+                foreach (var img in images) {
+                    Console.WriteLine("Processed {0}/{1} tables...", c, images.Length);
+
+                    DrawTable(ImageUtil.LoadImage(img)).Save(TableOutputDir + "/" + Path.GetFileName(img));
+
+                    c++;
+                }
+            });
+        }
+
+        static Bitmap DrawTable(Bitmap src) {
+            Bitmap res = new Bitmap(src);
+            var tableOpt = TableOCR.Program.RecognizeTable(src);
+            var g = Graphics.FromImage(res);
+            tableOpt.ForEach(table => {
+                table.DrawTable(g, new Pen(Brushes.Red, 2));
+            });
+            g.Dispose();
+            return res;
+        }
+    }
+}
