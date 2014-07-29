a5dfec2a8e809bb2ae970451198fe8cbeb93dcf9
Pronko <platon@pronko.ru>
2014-07-29 16:27:53 +0400

    improved Quality test output

--- QualityTest/Program.cs
+++ QualityTest/Program.cs
@@ -15,14 +15,14 @@ namespace QualityTest {
         static void Main(string[] args) {
             var digestSet = GradeDigestSet.ReadDefault();
 
-            List<GradeDigest> testDigests = LoadTestDigests();
+            List<Tuple<string, GradeDigest>> testDigests = LoadTestDigests();
             var gradePairs = new List<Tuple<GradeDigest, RecognitionResult>>();
             Util.Timed("compare with database", () => {
                 int c = 0;
                 foreach (var gd in testDigests) {
                     if (c % 100 == 0) Console.WriteLine("Processed {0}/{1} digests...", c, testDigests.Count);
 
-                    gradePairs.Add(new Tuple<GradeDigest, RecognitionResult>(gd, digestSet.FindBestMatch(gd)));
+                    gradePairs.Add(new Tuple<GradeDigest, RecognitionResult>(gd.Item2, digestSet.FindBestMatch(gd.Item2)));
 
                     c++;
                 }
@@ -30,7 +30,6 @@ namespace QualityTest {
 
             // output results
 
-            
             Func<byte, int> rights = g => gradePairs.Count(gp =>
                 gp.Item1.grade == gp.Item2.Digest.grade && 
                 gp.Item1.grade == g);
@@ -39,11 +38,11 @@ namespace QualityTest {
                 gp.Item1.grade == g);
             
 
-            Func<int, int> percentTotal = c => (int) Math.Floor((double) c / gradePairs.Count * 100);
-            Func<int, int, int> percent = (c, g) => {
+            Func<int, double> percentTotal = c => (double) c / gradePairs.Count * 100;
+            Func<int, int, double> percent = (c, g) => {
                 int tot = gradePairs.Count(gp => gp.Item1.grade == g);
                 if (tot > 0) {
-                    return (int) Math.Floor((double) c / tot * 100);
+                    return (double) c / tot * 100;
                 } else {
                     return 0;
                 }
@@ -54,12 +53,12 @@ namespace QualityTest {
             int rightTotal = gradePairs.Count(gp => gp.Item1.grade == gp.Item2.Digest.grade);
             int wrongTotal = gradePairs.Count(gp => gp.Item1.grade != gp.Item2.Digest.grade);
 
-            Console.WriteLine("Total (r/w): {0}/{1}, {2}/{3} %", 
+            Console.WriteLine("Total (r/w): {0}/{1}, {2:F1}/{3:F1} %", 
                 rightTotal, wrongTotal, 
                 percentTotal(rightTotal), percentTotal(wrongTotal));
 
             for (byte g = 2; g <= 5; g++) {
-                Console.WriteLine("{0}: {1}/{2}, {3}/{4} %", g,
+                Console.WriteLine("{0}: {1}/{2}, {3:F1}/{4:F1} %", g,
                     rights(g), wrongs(g), percent(rights(g), g), percent(wrongs(g), g));
             }
 
@@ -88,22 +87,27 @@ namespace QualityTest {
                     gp.Item2.Confidence > confidenceThreshold);
                 int unsureTotal = gradePairs.Count(gp => gp.Item2.Confidence <= confidenceThreshold);
 
-                Console.WriteLine("Total (r/w/u): {0}/{1}/{2}, {3}/{4}/{5} %",
+                Console.WriteLine("Total (r/w/u): {0}/{1}/{2}, {3:F1}/{4:F1}/{5:F1} %",
                     sureRightTotal, sureWrongTotal, unsureTotal,
                     percentTotal(sureRightTotal), percentTotal(sureWrongTotal), percentTotal(unsureTotal));
 
                 for (byte g = 2; g <= 5; g++) {
-                    Console.WriteLine("{0}: {1}/{2}/{3}, {4}/{5}/{6} %", g,
+                    Console.WriteLine("{0}: {1}/{2}/{3}, {4:F1}/{5:F1}/{6:F1} %", g,
                         sureRights(g), sureWrongs(g), unsure(g),
                         percent(sureRights(g), g), percent(sureWrongs(g), g), percent(unsure(g), g));
                 }
 
                 Console.WriteLine();
             }
+
+            Console.WriteLine("Recognition failures:");
+            gradePairs.Where(gp => gp.Item1.grade != gp.Item2.Digest.grade).Where(gp => gp.Item1.grade != 2).ToList().ForEach(gp => {
+                Console.WriteLine(testDigests.Find(gd => gd.Item2 == gp.Item1).Item1);
+            });
         }
 
-        public static List<GradeDigest> LoadTestDigests() {
-            List<GradeDigest> gradeDigests = new List<GradeDigest>();
+        public static List<Tuple<string, GradeDigest>> LoadTestDigests() {
+            List<Tuple<string, GradeDigest>> gradeDigests = new List<Tuple<string, GradeDigest>>();
 
             List<Tuple<string, byte>> gradeGroups = new List<Tuple<string, byte>> {
                 new Tuple<string, byte>(TestOcrData + "/grade-2/", 2),
@@ -131,7 +135,7 @@ namespace QualityTest {
                             GradeOCR.Program.NormalizeImage(
                                 ImageUtil.LoadImage(input.Item1)));
                     gd.grade = input.Item2;
-                    gradeDigests.Add(gd);
+                    gradeDigests.Add(new Tuple<string, GradeDigest>(input.Item1, gd));
 
                     c++;
                 }
